2377935374c3de9ca4e9ab780c23ed8b0f00c2c3
unknown <pingduzhou88@gmail.com>
2022-01-21 20:33:54 -0500

    login popup

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,93 +1,106 @@
-import { auth } from "./config.js"
+import { auth } from "./config.js";
 import {
-    connectAuthEmulator,
-    signInWithPopup,
-    GoogleAuthProvider,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    onAuthStateChanged,
-    FacebookAuthProvider,
-    TwitterAuthProvider,
-    signOut,
+  connectAuthEmulator,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  FacebookAuthProvider,
+  TwitterAuthProvider,
+  signOut,
 } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-auth.js";
-import {txtEmail,txtPassword,btnLogin,btnSignup, showApp, showLoginState, hideLoginError,showLoginError, showLoginForm, lblAuthState, googleloginbtn, facebookloginbtn, twitterloginbtn} from "./ui.js"
-
-
+import {
+  txtEmail,
+  txtPassword,
+  btnLogin,
+  btnSignup,
+  loginDiv,
+  showUserInfo,
+  hideLoginError,
+  showLoginError,
+  lblAuthState,
+  googleloginbtn,
+  facebookloginbtn,
+  twitterloginbtn,
+} from "./ui.js";
 
 const googleProvider = new GoogleAuthProvider();
 const facebookProvider = new FacebookAuthProvider();
 const twitterProvider = new TwitterAuthProvider();
-connectAuthEmulator(auth,"http://localhost:9099")
+connectAuthEmulator(auth, "http://localhost:9099");
 
-const createAccount= async() =>{
-    const signupEmail = txtEmail.value;
-    const signupPassword = txtPassword.value;
-    return createUserWithEmailAndPassword(auth, signupEmail, signupPassword);
-}
-btnSignup.addEventListener("click",createAccount);
+const createAccount = async () => {
+  const signupEmail = txtEmail.value;
+  const signupPassword = txtPassword.value;
+  return createUserWithEmailAndPassword(auth, signupEmail, signupPassword);
+};
+btnSignup.addEventListener("click", createAccount);
 
-
-const loginEmailPassword = async() => {
-    const loginemail = txtEmail.value;
-    const loginpassword = txtPassword.value;
-    try {
-           const userCredential = await signInWithEmailAndPassword(auth, loginemail, loginpassword);
-           window.location.replace("/TaxOnline.html")
-
+const loginEmailPassword = async () => {
+  const loginemail = txtEmail.value;
+  const loginpassword = txtPassword.value;
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      loginemail,
+      loginpassword
+    );
+    showUserInfo();
     console.log(userCredential.user);
-    } catch (error) {
-        console.log(error);
-        showLoginError(error);
-    }
-}
+    console.log(loginDiv);
+  } catch (error) {
+    console.log(error);
+    showLoginError(error);
+  }
+};
 
-btnLogin.addEventListener("click",loginEmailPassword);
+btnLogin.addEventListener("click", loginEmailPassword);
 
 export function logout() {
-    return signOut(auth);
+  return signOut(auth);
 }
 
 export function loginWithGoogle() {
-    return loginWithAuthProvider(googleProvider);
+  return loginWithAuthProvider(googleProvider);
 }
-googleloginbtn.addEventListener("click",loginWithGoogle);
+googleloginbtn.addEventListener("click", loginWithGoogle);
 export function loginWithFacebook() {
-    return loginWithAuthProvider(facebookProvider);
+  return loginWithAuthProvider(facebookProvider);
 }
-facebookloginbtn.addEventListener("click",loginWithFacebook);
+facebookloginbtn.addEventListener("click", loginWithFacebook);
 
 export function loginWithTwitter() {
-    return loginWithAuthProvider(twitterProvider);
+  return loginWithAuthProvider(twitterProvider);
 }
-twitterloginbtn.addEventListener("click",loginWithTwitter);
-
+twitterloginbtn.addEventListener("click", loginWithTwitter);
 
 export function loginWithAuthProvider(provider) {
-
-    signInWithPopup(auth, provider)
-        .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            // ...
-        })
-        .catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-        });
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
 }
-onAuthStateChanged(auth,user=>{
-    if(user){
-        console.log(user);
-    }else{
-        lblAuthState.innerHTML = "You're not logged in.";
+const monitorAuthState = async () => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+    } else {
     }
-})
+  });
+};
+monitorAuthState();
--- public/js/ui.js
+++ public/js/ui.js
@@ -1,47 +1,54 @@
 import { AuthErrorCodes } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-auth.js";
 
-export const txtEmail = document.querySelector('#txtEmail')
-export const txtPassword = document.querySelector('#txtPassword')
+export const txtEmail = document.querySelector("#txtEmail");
+export const txtPassword = document.querySelector("#txtPassword");
 
-export const btnLogin = document.querySelector('#btnLogin')
-export const btnSignup = document.querySelector('#btnSignup')
+export const btnLogin = document.querySelector("#btnLogin");
+export const btnSignup = document.querySelector("#btnSignup");
 
-export const btnLogout = document.querySelector('#btnLogout')
+export const btnLogout = document.querySelector("#btnLogout");
 
-export const divAuthState = document.querySelector('#divAuthState')
-export const lblAuthState = document.querySelector('#lblAuthState')
+export const divAuthState = document.querySelector("#divAuthState");
+export const lblAuthState = document.querySelector("#lblAuthState");
 
-export const divLoginError = document.querySelector('#divLoginError')
-export const lblLoginErrorMessage = document.querySelector('#lblLoginErrorMessage')
+export const divLoginError = document.querySelector("#divLoginError");
+export const lblLoginErrorMessage = document.querySelector(
+  "#lblLoginErrorMessage"
+);
 
+export const loginForm = document.querySelector("#loginform");
 
-export const loginForm = document.querySelector('#loginform')
+export const facebookloginbtn = document.querySelector(".facebook");
+export const twitterloginbtn = document.querySelector(".twitter");
+export const googleloginbtn = document.querySelector(".google");
 
-export const facebookloginbtn = document.querySelector('.facebook')
-export const twitterloginbtn = document.querySelector('.twitter')
-export const googleloginbtn = document.querySelector('.google')
+export const loginDiv = document.querySelector("#loginDiv");
+export const userInfo = document.querySelector("#userInfo");
 
-export const showLoginForm = () => {
-    loginForm.style.display = 'block'
-    loginInfo.style.display = 'none'
-}
-export const showApp = () => {
-    loginForm.style.display = 'none'
-    loginInfo.style.display = 'block'
-}
+export const showLoginDiv = () => {
+  loginDiv.classList.add("d-flex");
+  loginDiv.style.display = "block";
+  userInfo.style.display = "none";
+};
+export const showUserInfo = () => {
+  userInfo.style.display = "block";
+  loginDiv.classList.remove("d-flex");
+  loginDiv.style.display = "none";
+};
 export const hideLoginError = () => {
-    divLoginError.style.display = 'none'
-    lblLoginErrorMessage.innerHTML = ''
-}
+  divLoginError.style.display = "none";
+  lblLoginErrorMessage.innerHTML = "";
+};
 export const showLoginError = (error) => {
-    divLoginError.style.display = 'block'
-    if (error.code == AuthErrorCodes.INVALID_PASSWORD) {
-        lblLoginErrorMessage.innerHTML = `Wrong password. Try again.`
-    } else {
-        lblLoginErrorMessage.innerHTML = `Error: ${error.message}`
-    }
-}
+  divLoginError.style.display = "block";
+  if (error.code == AuthErrorCodes.INVALID_PASSWORD) {
+    lblLoginErrorMessage.innerHTML = `Wrong password. Try again.`;
+  } else {
+    lblLoginErrorMessage.innerHTML = `Error: ${error.message}`;
+  }
+};
 export const showLoginState = (user) => {
-    lblAuthState.innerHTML = `You're loggged in as ${user.displayName} (uid: ${user.uid},email: ${user.email})`
-}
-hideLoginError()
+  userInfo.innerHTML = `You're loggged in as ${user.displayName} (uid: ${user.uid},email: ${user.email})`;
+};
+
+hideLoginError();
