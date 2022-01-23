7f0108e9623f2329cc778756368227a77d9d631e
unknown <pingduzhou88@gmail.com>
2022-01-23 16:27:12 -0500

    submit
    ver0.1

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,53 @@ import {
 import { files } from "./ui.js";
 import { userEmail } from "./auth.js";
 
+function TfExcel() {
+  var data = [];
+  var data1 = [];
+  const taxform = [...document.querySelectorAll(".taxform")];
+  const liveform = [...document.querySelectorAll(".pd")];
+  taxform.forEach((i) => {
+    console.log(i.value);
+    if (i.value == "") {
+      window.alert("Please Input rhe required form!");
+    }
+    data.push(i.value);
+  });
+  liveform.forEach((j) => {
+    console.log(j.value);
+    if (j.value == "") {
+      //window.alert("Please Input rhe required form!");
+    }
+    data1.push(j.value);
+  });
+
+  downLoadExcel(data, data1, data[0] + ".csv");
+}
+function downLoadExcel(data, data1, fileName) {
+  //定义表头
+  let str = `First name,Last name,Date of Birth,SIN#,Email,Phone,Mailing Address,City,Province,Zip,Marital Status,First time tax filling in CA?,Are you a Canadian Citizen,Existing client\n`;
+  //增加\t为了不让表格显示科学计数法或者其他格式
+  for (let i = 0; i < data.length; i++) {
+    str += `${data[i] + "\t"},`;
+  }
+  str += `\nMailing Address,Moving in,Rental paid/Property tax,Landlord name,Residence Type\n`;
+  for (let j = 0; j < data1.length; j++) {
+    str += `${data1[j] + "\t"},`;
+    if (j % 5 == 4) {
+      str += `\n`;
+    }
+  }
+  console.log(str);
+  //console.log(userEmail);
+
+  const storageRef = ref(storage, userEmail + "/" + fileName);
+
+  uploadString(storageRef, str).then((snapshot) => {
+    console.log("Uploaded a blob or file!");
+  });
+}
 function uploadflie() {
+  TfExcel();
   files.forEach((file) => {
     const filename = file.files[0].name;
     const storageRef = ref(storage, userEmail + "/" + filename);
@@ -20,31 +66,4 @@ function uploadflie() {
 }
 document.getElementById("upload").addEventListener("click", uploadflie);
 
-function TfExcel() {
-  var data = [];
-  const taxform = [...document.querySelectorAll(".taxform")];
-  taxform.forEach((i) => {
-    console.log(i.value);
-    if (i.value == "") {
-      window.alert("Please Input rhe required form!");
-    }
-    data.push(i.value);
-  });
-
-  downLoadExcel(data, data[0] + ".csv");
-}
-function downLoadExcel(data, fileName) {
-  //定义表头
-  let str = `First name,Last name,Date of Birth,SIN#,Email,Phone,Mailing Address,City,Province,Zip,Marital Status,First time tax filling in CA?,Are you a Canadian Citizen,Existing client\n`;
-  //增加\t为了不让表格显示科学计数法或者其他格式
-  for (let i = 0; i < data.length; i++) {
-    str += `${data[i] + "\t"},`;
-  }
-  console.log(str);
-  const storageRef = ref(storage, fileName);
-  uploadString(storageRef, str).then((snapshot) => {
-    console.log("Uploaded a blob or file!");
-  });
-}
-
-document.getElementById("tfbtn").addEventListener("click", TfExcel);
+//document.getElementById("tfbtn").addEventListener("click", TfExcel);
